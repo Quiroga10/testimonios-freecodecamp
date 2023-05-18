d5e9e2525768f868707cbfaba46c8b40624ffef0
Quiroga10 <juniorquirogamio@gmail.com>
2023-05-17 22:00:18 -0500

    Testimonios FreeCodeCamp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.jsx';
+import testimonios from './data_testimonios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="App">
+        <div className='contenedor-principal'>
+          <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          {testimonios.map((testimonio, index) => (
+            <Testimonio
+              key={index}
+              nombre={testimonio.nombre}
+              pais={testimonio.pais}
+              imagen={testimonio.imagen}
+              cargo={testimonio.cargo}
+              empresa={testimonio.empresa}
+              testimonio={testimonio.testimonio}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/componentes/Testimonio.jsx
@@ -0,0 +1,27 @@
+import '../hojas-de-estilo/Testimonio.css';
+import React from "react";
+
+function Testimonio(props){
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)} //comillas invertidas para insertar codigo js dentro cadena de caracteres
+        alt={`Foto de ${props.nombre}`}
+      />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='texto-testimonio'>
+          "{props.testimonio}"
+        </p>
+      </div>
+    </div>
+  );
+}
+//exportacion por default para poder utilizarlo
+export default Testimonio;
